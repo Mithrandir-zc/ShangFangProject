a9b45d2d29fcf9c955a0df2403461be52e8d16b2
Mithrandir-zc <Mithrandir_zc>
2024-07-08 18:44:02 +0800

    index content changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
 /* Author: 
-
+    Mithrandir-zc
 */
-
+var lis = document.querySelector('list-gallery');
+var imglist = document.querySelector('gallery-images');
+for(var i=0; i<lis.children.length; i++){
+    var index = i;
+    lis.children[i].setAttribute('index',index);
+    lis.children[i].addEventListener('click',function(){
+        for(var j=0; j<imglist.children.length; j++){
+            var img = imglist.children[0].children[0];
+            img.src = "assets/images/gallery" + lis.children[i].getAttribute('index') + "/" + "img" + j + ".jpg";
+        }
+    });
+}
 
 
 
